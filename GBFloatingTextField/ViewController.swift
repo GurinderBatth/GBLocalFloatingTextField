9b46e17cf127521512992a2c1321241c03ccaf47
Apple <apple@apples-imac.local>
2018-10-04 21:51:37 +0530

    Initial Commit

---
+++ GBFloatingTextField/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GBFloatingTextField
+//
+//  Created by Apple on 04/10/18.
+//  Copyright © 2018 Batth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
